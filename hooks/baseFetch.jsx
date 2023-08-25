5b628a456b80428afcd4c0f6f51ee493c5d6cf9d
Gökdeniz Çetin <gokdenizcetins@gmail.com>
2023-08-25 17:58:10 +0300

    fix baseFetch func.

--- hooks/baseFetch.jsx
+++ hooks/baseFetch.jsx
@@ -1,16 +1,20 @@
 export default async function baseTefasFetch(endpoint, body) {
   try {
-    const response = await fetch(`https://www.tefas.gov.tr/api${endpoint}`, {
-      method: "POST",
-      body: body,
-      headers: {
-        "Accept-Encoding": "gzip",
-        "User-Agent": "okhttp/4.9.3",
-        "Content-Type": "application/x-www-form-urlencoded",
-        Connection: "Keep-Alive",
-        Host: "www.tefas.gov.tr",
-      },
-    });
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_TEFAS_BASE_URL}${endpoint}`,
+      {
+        cache: "no-cache",
+        method: "POST",
+        body: body,
+        headers: {
+          "Accept-Encoding": "gzip",
+          "User-Agent": "okhttp/4.9.3",
+          "Content-Type": "application/x-www-form-urlencoded",
+          Connection: "Keep-Alive",
+          Host: "www.tefas.gov.tr",
+        },
+      }
+    );
 
     if (response.ok) {
       const data = await response.json();
@@ -19,6 +23,6 @@ export default async function baseTefasFetch(endpoint, body) {
       return { error: response.status };
     }
   } catch (err) {
-    return { error: err.message };
+    return { error: err };
   }
 }
