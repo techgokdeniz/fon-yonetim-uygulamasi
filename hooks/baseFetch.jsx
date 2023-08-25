e141bbac8bf255df762e1e4edfd10e5435d7bc21
Gökdeniz Çetin <gokdenizcetins@gmail.com>
2023-08-25 17:52:29 +0300

    Fix: error api routes

--- hooks/baseFetch.jsx
+++ hooks/baseFetch.jsx
@@ -1,18 +1,19 @@
 export default async function baseTefasFetch(endpoint, body) {
-  const { NEXT_PUBLIC_TEFAS_BASE_URL } = process.env;
-
   try {
-    const response = await fetch(`${NEXT_PUBLIC_TEFAS_BASE_URL}${endpoint}`, {
-      method: "POST",
-      body: body,
-      headers: {
-        "Accept-Encoding": "gzip",
-        "User-Agent": "okhttp/4.9.3",
-        "Content-Type": "application/x-www-form-urlencoded",
-        Connection: "Keep-Alive",
-        Host: "www.tefas.gov.tr",
-      },
-    });
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_TEFAS_BASE_URL}${endpoint}`,
+      {
+        method: "POST",
+        body: body,
+        headers: {
+          "Accept-Encoding": "gzip",
+          "User-Agent": "okhttp/4.9.3",
+          "Content-Type": "application/x-www-form-urlencoded",
+          Connection: "Keep-Alive",
+          Host: "www.tefas.gov.tr",
+        },
+      }
+    );
 
     if (response.ok) {
       const data = await response.json();
