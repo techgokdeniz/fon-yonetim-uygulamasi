4b7bb1beead8ba63fe8e9809979011574e035259
Gökdeniz Çetin <gokdenizcetins@gmail.com>
2023-08-25 18:22:26 +0300

    Fix: fetch SSL error convert to axios

--- hooks/baseFetch.jsx
+++ hooks/baseFetch.jsx
@@ -1,28 +1,27 @@
+import axios from "axios";
+
 export default async function baseTefasFetch(endpoint, body) {
   try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_TEFAS_BASE_URL}${endpoint}`,
-      {
-        cache: "no-cache",
-        method: "POST",
-        body: body,
-        headers: {
-          "Accept-Encoding": "gzip",
-          "User-Agent": "okhttp/4.9.3",
-          "Content-Type": "application/x-www-form-urlencoded",
-          Connection: "Keep-Alive",
-          Host: "www.tefas.gov.tr",
-        },
-      }
-    );
+    const response = await axios({
+      method: "post",
+      url: `${process.env.NEXT_PUBLIC_TEFAS_BASE_URL}${endpoint}`,
+      data: body,
+      headers: {
+        "Accept-Encoding": "gzip",
+        "User-Agent": "okhttp/4.9.3",
+        "Content-Type": "application/x-www-form-urlencoded",
+        Connection: "Keep-Alive",
+        Host: "www.tefas.gov.tr",
+      },
+    });
 
-    if (response.ok) {
-      const data = await response.json();
-      return { data: data };
+    if (response.status === 200) {
+      const data = response.data;
+      return { data };
     } else {
       return { error: response.status };
     }
   } catch (err) {
-    return { error: err };
+    return { error: err.message };
   }
 }
