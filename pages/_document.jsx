40f3db874a7358cf05c4a7f1f6358bfacc29f326
Gökdeniz Çetin <gokdenizcetins@gmail.com>
2023-08-31 10:50:50 +0300

    edited landing page

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <title>Fon Yönetim Uygulaması</title>
-        <meta name="description" content="Fon Yönetim Uygulaması" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
